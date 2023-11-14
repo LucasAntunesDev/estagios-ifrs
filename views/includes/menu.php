01ed19df3c7900dd6a674258ebf273716916c63c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-14 05:57:55 -0300

    update: header quando o usuário não está logado

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -5,7 +5,8 @@ h-16 bg-[#127852] text-zinc-50 border-b-[3px] border-[#517174]">
 
 <img src="views/assets/IFRS W.png" alt="Logo do IFRS" class="h-10">
 
-<ul class="flex flex-row gap-x-4">
+<?php
+if(isset($_SESSION['usuario'])) echo '<ul class="flex flex-row gap-x-4">
 
     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
         <a href="livros.php">
@@ -33,9 +34,8 @@ h-16 bg-[#127852] text-zinc-50 border-b-[3px] border-[#517174]">
         <i class="fa-solid fa-arrow-right-from-bracket"></i>
         <a href="logout.php">Sair</a>
     </li>
-</ul>
-
-<?php 
+</ul>';
+ 
     echo isset($_SESSION['usuario']) ? "<div class='flex justify-center items-center gap-x-2'><i class='fa-solid fa-circle-user text-xl'></i> <span class='font-bold text-zinc-50 hover:text-zinc-200'>" .$_SESSION['usuario']->getNome() ."</span></div>" : 
     "<div class='flex justify-center items-center gap-x-2'><a href='login.php' class='font-bold hover:text-zinc-200'><i class='fa-solid fa-circle-user text-xl'></i> Identifique-se</a></div>" 
 ?>
