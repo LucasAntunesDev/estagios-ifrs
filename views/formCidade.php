8d3bfa5dec7fe34de96a279b9af6197cb430f379
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-17 18:57:34 -0300

    add: formulário de área, cidade e curso

---
+++ views/formCidade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estágios IFRS - Formulário de Cidade</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+    <?php include('views/includes/favicons.php');?>
+</head>
+
+<body>
+
+    <?php include('views/includes/menu.php'); ?>
+
+    <div class="flex flex-col justify-center items-center gap-2">
+        <h1 class="font-bold text-4xl text-[#C62126] my-4">
+            <i class="fa-solid fa-circle-user"></i>
+            Formulário de Cidade
+        </h1>
+        <a href="cidades.php" class="text-gray-900 font-bold hover:text-gray-700">Voltar para a listagem</a>
+
+        <fieldset class="border-[1px] border-zinc-300 p-10 rounded-md w-[40rem]">
+            <legend class="text-gray-900 font-bold text-center text-xl">Dados do Cidade</legend>
+
+            <form action="salvarCidade.php" method="post" class="flex flex-col justify-center items-center gap-2">
+                <input type="hidden" name="id" value='<?php echo $cidade->getId(); ?>'>
+
+                <div>
+                    <label for="nome" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Nome</label>
+                    <input type="text" id="nome" name="nome" value='<?php echo $cidade->getNome(); ?>' class="rounded-md 
+                border-0 py-1.5 px-7 text-gray-900 ring-1 ring-inset ring-gray-500 
+                focus:ring-2 focus:ring-inset focus:ring-[#ef5d58] outline-none text-zinc-800">
+                </div>
+
+                <div>
+                    <button type="submit" class="bg-emerald-600 rounded-full py-1 px-24 hover:bg-emerald-800 text-zinc-50 flex items-center mt-4">
+                        <span class="material-symbols-outlined">
+                            add
+                        </span>
+                        Salvar
+                    </button>
+            </form>
+        </fieldset>
+
+        </form>
+    </div>
+
+</body>
+
+</html>
