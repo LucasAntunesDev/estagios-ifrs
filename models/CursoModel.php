0274ef36e7e4cdd0fda984a76ee406a3e3e722d5
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-17 18:21:50 -0300

    add: CursoModel.php

---
+++ models/CursoModel.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Model;
+
+use Model\VO\CursoVO;
+use Util\Database;
+
+final class CursoModel extends Model {
+    public function selectAll($vo = null) {
+        $db = new Database();
+        $data = $db->select('SELECT * FROM curso');
+
+        $array = [];
+
+        foreach ($data as $row) {
+            $vo = new CursoVO($row['id'], $row['nome'], $row['id_coordenador']);
+            array_push($array, $vo);
+        }
+
+        return $array;
+    }
+    public function selectOne($vo = null) {
+        $db = new Database();
+        $query = 'SELECT * FROM curso WHERE id = :id';
+        $binds = [':id' => $vo->getId()];
+        $data = $db->select($query, $binds);
+
+        if (count($data) === 0) return null;
+
+        return new CursoVO(
+            $data[0]['id'],
+            $data[0]['nome'],
+            $data[0]['id_coordenador']
+        );
+    }
+
+    public function insert($vo = null) {
+        $db = new Database();
+        $query = 'INSERT INTO curso
+        (nome, id_coordenador) 
+        VALUES 
+        (:nome, :id_coordenador)';
+
+        $binds = [
+            ':nome' => $vo->getNome(),
+            ':id_coordenador' => $vo->getIdCoordenador()
+        ];
+
+        $success = $db->execute($query, $binds);
+
+        return $success ? $db->getLastInsertedId() : null;
+    }
+
+    public function update($vo = null) {
+        $db = new Database();
+        $binds = [
+            ':nome' => $vo->getNome(),
+            ':id_coordenador' => $vo->getIdCoordenador()
+        ];
+
+        $query = 'UPDATE curso
+                    SET nome = :nome,
+                    id_coordenador = :id_coordenador
+                    WHERE id = :id';
+
+
+        return $db->execute($query, $binds);
+    }
+
+    public function delete($vo = null) {
+        $db = new Database();
+        $query = 'DELETE FROM curso WHERE id = :id';
+        $binds = [':id' => $vo->getId()];
+
+        return $db->execute($query, $binds);
+    }
+}
