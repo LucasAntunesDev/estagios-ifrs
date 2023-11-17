40c1b6e3b0413e896ae935a6f8c100c1635234e6
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-17 12:05:55 -0300

    update: removidos links no header

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -9,24 +9,24 @@ transition ease-in-out duration-300 cursor-pointer">
     </a>
 
     <?php
-    if (isset($_SESSION['usuario'])) echo '<ul class="flex flex-row gap-x-4">
+//     if (isset($_SESSION['usuario'])) echo '<ul class="flex flex-row gap-x-4">
 
     
-    <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-        <i class="fa-solid fa-graduation-cap"></i>
-        <a href="alunos.php">Alunos</a>
-    </li>
+//     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+//         <i class="fa-solid fa-graduation-cap"></i>
+//         <a href="alunos.php">Alunos</a>
+//     </li>
 
-    <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-        <i class="fa-solid fa-circle-user"></i>
-        <a href="usuarios.php">Usuarios</a>
-    </li>
+//     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+//         <i class="fa-solid fa-circle-user"></i>
+//         <a href="usuarios.php">Usuarios</a>
+//     </li>
     
-    <li class="w-fit flex hover:text-red-600 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-        <i class="fa-solid fa-arrow-right-from-bracket"></i>
-        <a href="logout.php">Sair</a>
-    </li>
-</ul>';
+//     <li class="w-fit flex hover:text-red-600 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+//         <i class="fa-solid fa-arrow-right-from-bracket"></i>
+//         <a href="logout.php">Sair</a>
+//     </li>
+// </ul>';
 
     echo isset($_SESSION['usuario']) ? "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'><i class='fa-solid fa-circle-user text-xl'></i> <span class='font-bold text-zinc-50'>" . $_SESSION['usuario']->getNome() . "</span></div>" :
         "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'><a href='login.php' class='font-bold hover:text-zinc-200'><i class='fa-solid fa-circle-user text-xl'></i> Identifique-se</a></div>"
