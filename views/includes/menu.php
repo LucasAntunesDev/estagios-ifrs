7691ea1a10416f905e6e1ba98afa6c13f5881c99
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-18 11:18:54 -0300

    style: dropdown menu com os módulos no header

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -9,27 +9,196 @@ transition ease-in-out duration-300 cursor-pointer">
     </a>
 
     <?php
-//     if (isset($_SESSION['usuario'])) echo '<ul class="flex flex-row gap-x-4">
+    //     if (isset($_SESSION['usuario'])) echo '<ul class="flex flex-row gap-x-4">
 
-    
-//     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-//         <i class="fa-solid fa-graduation-cap"></i>
-//         <a href="alunos.php">Alunos</a>
-//     </li>
 
-//     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-//         <i class="fa-solid fa-circle-user"></i>
-//         <a href="usuarios.php">Usuarios</a>
-//     </li>
-    
-//     <li class="w-fit flex hover:text-red-600 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
-//         <i class="fa-solid fa-arrow-right-from-bracket"></i>
-//         <a href="logout.php">Sair</a>
-//     </li>
-// </ul>';
+    //     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+    //         <i class="fa-solid fa-graduation-cap"></i>
+    //         <a href="alunos.php">Alunos</a>
+    //     </li>
 
-    echo isset($_SESSION['usuario']) ? "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'><i class='fa-solid fa-circle-user text-xl'></i> <span class='font-bold text-zinc-50'>" . $_SESSION['usuario']->getNome() . "</span></div>" :
-        "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'><a href='login.php' class='font-bold hover:text-zinc-200'><i class='fa-solid fa-circle-user text-xl'></i> Identifique-se</a></div>"
+    //     <li class="w-fit flex hover:text-zinc-200 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+    //         <i class="fa-solid fa-circle-user"></i>
+    //         <a href="usuarios.php">Usuarios</a>
+    //     </li>
+
+    //     <li class="w-fit flex hover:text-red-600 transition duration-300 ease-in-out items-center gap-x-1 font-bold">
+    //         <i class="fa-solid fa-arrow-right-from-bracket"></i>
+    //         <a href="logout.php">Sair</a>
+    //     </li>
+    // </ul>';
+    echo '<div class="flex items-center gap-x-4">';
+
+    if (isset($_SESSION['usuario'])) {
+        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'>
+                <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' class='w-6 h-6'>
+                    <path fill-rule='evenodd' d='M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 
+                    6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486
+                     7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75
+                      0 11-7.5 0 3.75 3.75 0 017.5 0z' clip-rule='evenodd' />
+                </svg>
+
+                <span class='font-bold text-zinc-50'>" . $_SESSION['usuario']->getNome() . "</span>
+        </div>";
+    } else {
+        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'>
+        <a href='login.php' class='flex gap-x-2 font-bold hover:text-zinc-200'>
+        
+        <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' class='w-6 h-6'>
+            <path fill-rule='evenodd' d='M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 
+            6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54
+            -1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0
+             01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z' clip-rule='evenodd' />
+        </svg>
+
+            </i> Identifique-se</a>
+        </div>";
+    }
     ?>
+    <div class="<?php echo isset($_SESSION['usuario']) ? '' : 'hidden' ?>">
+
+        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="hover:scale-110 transition duration-300
+    ease-in-out" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                <path fill-rule="evenodd" d="M3 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 5.25zm0 4.5A.75.75 0 013.75 9h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 9.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75zm0 4.5a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" />
+            </svg>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+            <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="usuarios.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z" clip-rule="evenodd" />
+                        </svg>
+                        Usuários
+                    </a>
+                </li>
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+
+                    <a class="flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="alunos.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M9.664 1.319a.75.75 0 01.672 0 41.059 41.059 0 018.198 5.424.75.75 0 01-.254 1.285 31.372 31.372 0 00-7.86 3.83.75.75 0 01-.84 0 31.508 31.508 0 00-2.08-1.287V9.394c0-.244.116-.463.302-.592a35.504 35.504 0 013.305-2.033.75.75 0 00-.714-1.319 37 37 0 00-3.446 2.12A2.216 2.216 0 006 9.393v.38a31.293 31.293 0 00-4.28-1.746.75.75 0 01-.254-1.285 41.059 41.059 0 018.198-5.424zM6 11.459a29.848 29.848 0 00-2.455-1.158 41.029 41.029 0 00-.39 3.114.75.75 0 00.419.74c.528.256 1.046.53 1.554.82-.21.324-.455.63-.739.914a.75.75 0 101.06 1.06c.37-.369.69-.77.96-1.193a26.61 26.61 0 013.095 2.348.75.75 0 00.992 0 26.547 26.547 0 015.93-3.95.75.75 0 00.42-.739 41.053 41.053 0 00-.39-3.114 29.925 29.925 0 00-5.199 2.801 2.25 2.25 0 01-2.514 0c-.41-.275-.826-.541-1.25-.797a6.985 6.985 0 01-1.084 3.45 26.503 26.503 0 00-1.281-.78A5.487 5.487 0 006 12v-.54z" clip-rule="evenodd" />
+                        </svg>
+                        Alunos
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="estagiosAlunos.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clip-rule="evenodd" />
+                            <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
+                        </svg>
+                        Estagios Alunos
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="supervisores.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path d="M10 9a3 3 0 100-6 3 3 0 000 6zM6 8a2 2 0 11-4 0 2 2 0 014 0zM1.49 15.326a.78.78 0 01-.358-.442 3 3 0 014.308-3.516 6.484 6.484 0 00-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 01-2.07-.655zM16.44 15.98a4.97 4.97 0 002.07-.654.78.78 0 00.357-.442 3 3 0 00-4.308-3.517 6.484 6.484 0 011.907 3.96 2.32 2.32 0 01-.026.654zM18 8a2 2 0 11-4 0 2 2 0 014 0zM5.304 16.19a.844.844 0 01-.277-.71 5 5 0 019.947 0 .843.843 0 01-.277.71A6.975 6.975 0 0110 18a6.974 6.974 0 01-4.696-1.81z" />
+                        </svg>
+                        Supervisor
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="professores.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path d="M10 9a3 3 0 100-6 3 3 0 000 6zM6 8a2 2 0 11-4 0 2 2 0 014 0zM1.49 15.326a.78.78 0 01-.358-.442 3 3 0 014.308-3.516 6.484 6.484 0 00-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 01-2.07-.655zM16.44 15.98a4.97 4.97 0 002.07-.654.78.78 0 00.357-.442 3 3 0 00-4.308-3.517 6.484 6.484 0 011.907 3.96 2.32 2.32 0 01-.026.654zM18 8a2 2 0 11-4 0 2 2 0 014 0zM5.304 16.19a.844.844 0 01-.277-.71 5 5 0 019.947 0 .843.843 0 01-.277.71A6.975 6.975 0 0110 18a6.974 6.974 0 01-4.696-1.81z" />
+                        </svg>
+                        Professor
+                    </a>
+                </li>
+
+                <div class="my-3 py-2 border-y-[1px] border-y-neutral-300">
+                    <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                        <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="TCCS.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z" clip-rule="evenodd" />
+                            </svg>
+                            TCC
+                        </a>
+                    </li>
+
+                    <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                        <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="planoAtividades.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M3 3.5A1.5 1.5 0 014.5 2h6.879a1.5 1.5 0 011.06.44l4.122 4.12A1.5 1.5 0 0117 7.622V16.5a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 013 16.5v-13zm10.857 5.691a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 00-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                            </svg>
+                            Plano de atividades
+                        </a>
+                    </li>
+
+                    <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                        <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="avaliacaoEmpresas.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M3 3.5A1.5 1.5 0 014.5 2h6.879a1.5 1.5 0 011.06.44l4.122 4.12A1.5 1.5 0 0117 7.622V16.5a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 013 16.5v-13zm10.857 5.691a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 00-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                            </svg>
+                            Avaliação da empresa
+                        </a>
+                    </li>
+
+                    <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                        <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="autoAvaliacoes.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                <path fill-rule="evenodd" d="M3 3.5A1.5 1.5 0 014.5 2h6.879a1.5 1.5 0 011.06.44l4.122 4.12A1.5 1.5 0 0117 7.622V16.5a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 013 16.5v-13zm10.857 5.691a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 00-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" />
+                            </svg>
+                            Autoavaliação
+                        </a>
+                    </li>
+                </div>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="empresas.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M1 2.75A.75.75 0 011.75 2h10.5a.75.75 0 010 1.5H12v13.75a.75.75 0 01-.75.75h-1.5a.75.75 0 01-.75-.75v-2.5a.75.75 0 00-.75-.75h-2.5a.75.75 0 00-.75.75v2.5a.75.75 0 01-.75.75h-2.5a.75.75 0 010-1.5H2v-13h-.25A.75.75 0 011 2.75zM4 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM4.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM8 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM8.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM14.25 6a.75.75 0 00-.75.75V17a1 1 0 001 1h3.75a.75.75 0 000-1.5H18v-9h.25a.75.75 0 000-1.5h-4zm.5 3.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zm.5 3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1z" clip-rule="evenodd" />
+                        </svg>
+                        Empresa
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="cidades.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path d="M14.916 2.404a.75.75 0 01-.32 1.012l-.596.31V17a1 1 0 01-1 1h-2.26a.75.75 0 01-.75-.75v-3.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.5a.75.75 0 01-.75.75h-3.5a.75.75 0 010-1.5H2V9.957a.75.75 0 01-.596-1.372L2 8.275V5.75a.75.75 0 011.5 0v1.745l10.404-5.41a.75.75 0 011.012.32zM15.861 8.57a.75.75 0 01.736-.025l1.999 1.04A.75.75 0 0118 10.957V16.5h.25a.75.75 0 110 1.5h-2a.75.75 0 01-.75-.75V9.21a.75.75 0 01.361-.64z" />
+                        </svg>
+                        Cidade
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="cursos.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M9.664 1.319a.75.75 0 01.672 0 41.059 41.059 0 018.198 5.424.75.75 0 01-.254 1.285 31.372 31.372 0 00-7.86 3.83.75.75 0 01-.84 0 31.508 31.508 0 00-2.08-1.287V9.394c0-.244.116-.463.302-.592a35.504 35.504 0 013.305-2.033.75.75 0 00-.714-1.319 37 37 0 00-3.446 2.12A2.216 2.216 0 006 9.393v.38a31.293 31.293 0 00-4.28-1.746.75.75 0 01-.254-1.285 41.059 41.059 0 018.198-5.424zM6 11.459a29.848 29.848 0 00-2.455-1.158 41.029 41.029 0 00-.39 3.114.75.75 0 00.419.74c.528.256 1.046.53 1.554.82-.21.324-.455.63-.739.914a.75.75 0 101.06 1.06c.37-.369.69-.77.96-1.193a26.61 26.61 0 013.095 2.348.75.75 0 00.992 0 26.547 26.547 0 015.93-3.95.75.75 0 00.42-.739 41.053 41.053 0 00-.39-3.114 29.925 29.925 0 00-5.199 2.801 2.25 2.25 0 01-2.514 0c-.41-.275-.826-.541-1.25-.797a6.985 6.985 0 01-1.084 3.45 26.503 26.503 0 00-1.281-.78A5.487 5.487 0 006 12v-.54z" clip-rule="evenodd" />
+                        </svg>
+                        Curso
+                    </a>
+                </li>
+
+                <li class="hover:bg-zinc-100 rounded-md py-1 mx-2">
+                    <a class="block flex items-center gap-x-2 py-1 px-3 text-neutral-700  font-bold" href="areas.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M1 2.75A.75.75 0 011.75 2h16.5a.75.75 0 010 1.5H18v8.75A2.75 2.75 0 0115.25 15h-1.072l.798 3.06a.75.75 0 01-1.452.38L13.41 18H6.59l-.114.44a.75.75 0 01-1.452-.38L5.823 15H4.75A2.75 2.75 0 012 12.25V3.5h-.25A.75.75 0 011 2.75zM7.373 15l-.391 1.5h6.037l-.392-1.5H7.373zM13.25 5a.75.75 0 01.75.75v5.5a.75.75 0 01-1.5 0v-5.5a.75.75 0 01.75-.75zm-6.5 4a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 016.75 9zm4-1.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z" clip-rule="evenodd" />
+                        </svg>
+                        Área
+                    </a>
+                </li>
+
+
+                <li class="mt-3 border-t-[1px] border-t-neutral-300">
+                    <a class="mt-2 flex items-center gap-x-2 block py-1 px-3 text-red-500 hover:text-red-800 font-bold" href="logout.php">
+                        Sair
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M7.5 3.75A1.5 1.5 0 006 5.25v13.5a1.5 1.5 0 001.5 1.5h6a1.5 1.5 0 001.5-1.5V15a.75.75 0 011.5 0v3.75a3 3 0 01-3 3h-6a3 3 0 01-3-3V5.25a3 3 0 013-3h6a3 3 0 013 3V9A.75.75 0 0115 9V5.25a1.5 1.5 0 00-1.5-1.5h-6zm5.03 4.72a.75.75 0 010 1.06l-1.72 1.72h10.94a.75.75 0 010 1.5H10.81l1.72 1.72a.75.75 0 11-1.06 1.06l-3-3a.75.75 0 010-1.06l3-3a.75.75 0 011.06 0z" clip-rule="evenodd" />
+                        </svg>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 </div>
