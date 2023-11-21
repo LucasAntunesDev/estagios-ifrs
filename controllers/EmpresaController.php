d6576f3516a35c5dd826f33b69bcffb26b254b0d
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-21 19:49:59 -0300

    add: EmpresaController

---
+++ controllers/EmpresaController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Controller;
+
+use Embed\Http\Redirects;
+use Model\EmpresaModel;
+use Model\VO\EmpresaVO;
+
+
+final class EmpresaController extends Controller {
+    public function list() {
+        $model  = new EmpresaModel();
+        $data = $model->selectAll();
+
+        $this->loadView('listaEmpresas', [
+            'empresas' => $data
+        ]);
+    }
+
+    public function get() {
+        $id = $_GET['id'] ?? null; #>= php 8
+        $id = (isset($_GET['id'])) ? $_GET['id'] : null; # < php 8
+
+        if (empty($id)) {
+            $vo = new EmpresaVO();
+        } else {
+            $model = new EmpresaModel();
+            $vo = $model->selectOne(new EmpresaVO($id));
+        }
+
+        $this->loadView('formEmpresa', ['empresa' => $vo]);
+    }
+
+    public function save() {
+        $id = $_POST['id'];
+        $vo = new EmpresaVO(
+            $_POST['id'],
+            $_POST['nome'],
+            $_POST['endereco'],
+            $_POST['telefone'],
+            $_POST['email'],
+            $_POST['cnpj'],
+            $_POST['id_cidade']
+        );
+        $model = new EmpresaModel();
+
+        $return = empty($id) ? $model->insert($vo) : $model->update($vo);
+
+        $this->redirect('empresas.php');
+    }
+
+    public function remove() {
+        if (empty($_GET['id'])) die('Necessário passar o ID');
+
+        $model = new EmpresaModel();
+
+        $return = $model->delete(new EmpresaVO($_GET['id']));
+
+        $this->redirect('empresas.php');
+    }
+}
