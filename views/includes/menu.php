147b50ddca6a5ef5ddc18d92a1bce91ea5967362
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-11-26 16:40:56 -0300

    update: menu.php

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -11,7 +11,7 @@ transition ease-in-out duration-300 cursor-pointer">
     echo '<div class="flex items-center gap-x-4">';
 
     if (isset($_SESSION['usuario'])) {
-        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'>
+        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-300 hover:cursor-pointer'>
                 <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' class='w-6 h-6'>
                     <path fill-rule='evenodd' d='M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 
                     6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486
@@ -22,8 +22,8 @@ transition ease-in-out duration-300 cursor-pointer">
                 <span class='font-bold text-zinc-50'>" . $_SESSION['usuario']->getNome() . "</span>
         </div>";
     } else {
-        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-200 hover:cursor-pointer'>
-        <a href='login.php' class='flex gap-x-2 font-bold hover:text-zinc-200'>
+        echo "<div class='flex justify-center items-center gap-x-2 hover:text-zinc-300 hover:cursor-pointer'>
+        <a href='login.php' class='flex gap-x-2 font-bold hover:text-zinc-300'>
         
         Entrar
         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor' class='w-6 h-6'>
@@ -71,7 +71,7 @@ transition ease-in-out duration-300 cursor-pointer">
                             <path fill-rule="evenodd" d="M6 3.75A2.75 2.75 0 018.75 1h2.5A2.75 2.75 0 0114 3.75v.443c.572.055 1.14.122 1.706.2C17.053 4.582 18 5.75 18 7.07v3.469c0 1.126-.694 2.191-1.83 2.54-1.952.599-4.024.921-6.17.921s-4.219-.322-6.17-.921C2.694 12.73 2 11.665 2 10.539V7.07c0-1.321.947-2.489 2.294-2.676A41.047 41.047 0 016 4.193V3.75zm6.5 0v.325a41.622 41.622 0 00-5 0V3.75c0-.69.56-1.25 1.25-1.25h2.5c.69 0 1.25.56 1.25 1.25zM10 10a1 1 0 00-1 1v.01a1 1 0 001 1h.01a1 1 0 001-1V11a1 1 0 00-1-1H10z" clip-rule="evenodd" />
                             <path d="M3 15.055v-.684c.126.053.255.1.39.142 2.092.642 4.313.987 6.61.987 2.297 0 4.518-.345 6.61-.987.135-.041.264-.089.39-.142v.684c0 1.347-.985 2.53-2.363 2.686a41.454 41.454 0 01-9.274 0C3.985 17.585 3 16.402 3 15.055z" />
                         </svg>
-                        Estagios Alunos
+                        Estagios
                     </a>
                 </li>
 
