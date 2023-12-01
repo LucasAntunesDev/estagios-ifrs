4f3efecc59898ecfd8dc8293fa6c4b431fcd5b09
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-12-01 10:50:09 -0300

    update: formulário de supervisor

--- views/formSupervisor.php
+++ views/formSupervisor.php
@@ -61,86 +61,91 @@
         <fieldset class="border-[1px] border-zinc-300 p-10 rounded-md w-[40rem]">
             <legend class="text-gray-900 font-bold text-center text-xl">Dados do Supervisor</legend>
 
-            <form action="salvarSupervisor.php" method="post" class="flex flex-col justify-center items-center gap-2">
+            <form action="salvarSupervisor.php" method="post" class="flex flex-col justify-center items-start gap-2">
                 <input type="hidden" name="id" value='<?php echo $supervisor->getId(); ?>'>
 
-                <div>
-                    <label for="nome" class="block text-sm font-medium leading-6 text-neutral-700 mb-2">Nome</label>
-                    <div class="relative">
-                        <input type="text" id="nome" name="nome" value='<?php echo $supervisor->getNome(); ?>' class="rounded-md 
+                <div class="flex gap-x-4">
+                    <div>
+                        <label for="nome" class="block text-sm font-medium leading-6 text-neutral-700 mb-2">Nome</label>
+                        <div class="relative">
+                            <input type="text" id="nome" name="nome" value='<?php echo $supervisor->getNome(); ?>' class="rounded-md 
                 border-0 py-1.5 pr-7 pl-10 text-neutral-700 ring-1 ring-inset ring-gray-500 
                 focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none ">
 
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
-                                <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-                            </svg>
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
+                                    <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+                                </svg>
 
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div>
-                    <label for="telefone" class="block text-sm font-medium leading-6 text-neutral-700 mb-2">Telefone</label>
+                    <div>
+                        <label for="id_empresa" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Empresa</label>
 
-                    <div class="relative">
-                        <input type="number" id="telefone" name="telefone" value='<?php echo $supervisor->getTelefone(); ?>' class="rounded-md 
-                border-0 py-1.5 pr-7 pl-10 text-neutral-700 ring-1 ring-inset ring-gray-500 
-                focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none">
-
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
-                                <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
-                            </svg>
-                        </div>
-
-                    </div>
-
-                </div>
-
-                <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Email</label>
-
-                    <div class="relative">
-                        <input type="email" id="email" name="email" value='<?php echo $supervisor->getEmail(); ?>' class="rounded-md 
-                border-0 py-1.5 pr-7 pl-10 text-neutral-700 ring-1 ring-inset ring-gray-500 
-                focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none">
-
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
-                                <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
-                                <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
-                            </svg>
-                        </div>
-
-                    </div>
-
-                </div>
-
-
-                <div>
-                    <label for="id_empresa" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Empresa</label>
-
-                    <div class="relative">
-                        <select id="id_empresa" name="id_empresa" value="id_empresa" class="rounded-md 
+                        <div class="relative">
+                            <select id="id_empresa" name="id_empresa" value="id_empresa" class="rounded-md 
                     border-0 py-1.5 pr-7 pl-10 text-gray-900 ring-1 ring-inset ring-gray-500 
                     focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none">
-                            <?php
-                            foreach ($empresas as $empresa) {
-                                $selected = ($empresa->getId() == $supervisor->getIdEmpresa()) ? "selected" : "";
-                                echo "<option value='" . $empresa->getId() . "' " . $selected . "> " . $empresa->getNome() . "</option>";
-                            }
-                            ?>
-                        </select>
+                                <?php
+                                foreach ($empresas as $empresa) {
+                                    $selected = ($empresa->getId() == $supervisor->getIdEmpresa()) ? "selected" : "";
+                                    echo "<option value='" . $empresa->getId() . "' " . $selected . "> " . $empresa->getNome() . "</option>";
+                                }
+                                ?>
+                            </select>
 
-                        <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
-                                <path fill-rule="evenodd" d="M4 16.5v-13h-.25a.75.75 0 010-1.5h12.5a.75.75 0 010 1.5H16v13h.25a.75.75 0 010 1.5h-3.5a.75.75 0 01-.75-.75v-2.5a.75.75 0 00-.75-.75h-2.5a.75.75 0 00-.75.75v2.5a.75.75 0 01-.75.75h-3.5a.75.75 0 010-1.5H4zm3-11a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM7.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM11 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zm.5 3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1z" clip-rule="evenodd" />
-                            </svg>
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
+                                    <path fill-rule="evenodd" d="M4 16.5v-13h-.25a.75.75 0 010-1.5h12.5a.75.75 0 010 1.5H16v13h.25a.75.75 0 010 1.5h-3.5a.75.75 0 01-.75-.75v-2.5a.75.75 0 00-.75-.75h-2.5a.75.75 0 00-.75.75v2.5a.75.75 0 01-.75.75h-3.5a.75.75 0 010-1.5H4zm3-11a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM7.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM11 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zm.5 3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1z" clip-rule="evenodd" />
+                                </svg>
 
+                            </div>
                         </div>
                     </div>
+
                 </div>
+
+                <div class="flex gap-x-4">
+                    <div>
+                        <label for="telefone" class="block text-sm font-medium leading-6 text-neutral-700 mb-2">Telefone</label>
+
+                        <div class="relative">
+                            <input type="number" id="telefone" name="telefone" value='<?php echo $supervisor->getTelefone(); ?>' class="rounded-md 
+                border-0 py-1.5 pr-7 pl-10 text-neutral-700 ring-1 ring-inset ring-gray-500 
+                focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none">
+
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
+                                    <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Email</label>
+
+                        <div class="relative">
+                            <input type="email" id="email" name="email" value='<?php echo $supervisor->getEmail(); ?>' class="rounded-md 
+                border-0 py-1.5 pr-7 pl-10 text-neutral-700 ring-1 ring-inset ring-gray-500 
+                focus:ring-2 focus:ring-inset focus:ring-vermelho outline-none">
+
+                            <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 fill-neutral-700">
+                                    <path d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z" />
+                                    <path d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z" />
+                                </svg>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
 
                 <div>
                     <label for="cargo" class="block text-sm font-medium leading-6 text-gray-900 mb-2">Cargo</label>
@@ -176,7 +181,7 @@
         </form>
     </div>
 
-    <?php include_once('views/includes/footer.php')?>
+    <?php include_once('views/includes/footer.php') ?>
 
 </body>
 
