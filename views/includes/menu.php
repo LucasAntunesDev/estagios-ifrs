ca357a90b47f75c11149e05c9f6051cfb218119c
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2023-12-03 08:46:05 -0300

    fix: link para página do usuário no header

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -112,7 +112,7 @@ transition ease-in-out duration-300 cursor-pointer">
                 <!-- Dropdown menu -->
                 <div class='z-50 hidden my-1 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow' id='user-dropdown'>
                     
-                    <a href='usuario.php?id='" . $_SESSION['usuario']->getId() . "'>
+                    <a href='usuario.php?id=" . $_SESSION['usuario']->getId() . "'>
                         <div class='flex items-center gap-x-1 px-4 py-2 hover:bg-zinc-100 rounded-md m-2'>
                             <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='currentColor' class='w-7 h-7 fill-neutral-900'>
                                 <path fill-rule='evenodd' d='M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-5.5-2.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 12a5.99 5.99 0 00-4.793 2.39A6.483 6.483 0 0010 16.5a6.483 6.483 0 004.793-2.11A5.99 5.99 0 0010 12z' clip-rule='evenodd' />
