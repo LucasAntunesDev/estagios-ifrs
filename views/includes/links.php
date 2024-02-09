a49fd223b4922fe3389bdb0e968aa84b4ffd5063
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-02-09 08:44:01 -0300

    update: criação de uma pasta 'js dentro de 'assets'

--- views/includes/links.php
+++ views/includes/links.php
@@ -13,8 +13,8 @@
 <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="views/includes/jquery.mask.js"></script>
-<script src="views/includes/script.js"></script>
+<script src="views/assets/js/jquery.mask.js"></script>
+<script src="views/assets/js/script.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js" integrity="sha512-eyHL1atYNycXNXZMDndxrDhNAegH2BDWt1TmkXJPoGf1WLlNYt08CSjkqF5lnCRmdm3IrkHid8s2jOUY4NIZVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
