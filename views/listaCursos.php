3a4aae6ca8380aa3a964a9466d6fa476aeb7bd6f
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-02-09 09:17:37 -0300

    update: remoção de links não utilizados nas views

--- views/listaCursos.php
+++ views/listaCursos.php
@@ -5,9 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estágios IFRS - Cursos</title>
-    
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <?php include('views/includes/links.php'); ?>
 </head>
 
