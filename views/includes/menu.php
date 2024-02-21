57bc60f225aab24832ee01057e7ed4ecd84bc6bf
Lucas Antunes <104913618+LucasAntunes06@users.noreply.github.com>
2024-02-21 06:18:23 -0300

    update: todos os links no menu com nome no plural

--- views/includes/menu.php
+++ views/includes/menu.php
@@ -76,7 +76,7 @@ transition ease-in-out duration-300 cursor-pointer">
                     <ul class="py-2 text-sm fill-verde-400" aria-labelledby="dropdownDefaultButton">
 
                         <div class="md:hidden">
-                            <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                            <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                                 <a href="estagiosAlunos.php"
                                     class="flex items-center gap-x-2 py-1 px-3 fill-verde-400 font-bold">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -91,7 +91,7 @@ transition ease-in-out duration-300 cursor-pointer">
                                 </a>
                             </li>
 
-                            <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                            <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                                 <a href="alunos.php"
                                     class="flex items-center gap-x-2 py-1 px-3 fill-verde-400 font-bold">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -104,7 +104,7 @@ transition ease-in-out duration-300 cursor-pointer">
                                 </a>
                             </li>
 
-                            <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                            <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                                 <a href="professores.php"
                                     class="flex items-center gap-x-2 py-1 px-3 fill-verde-400 font-bold">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -117,7 +117,7 @@ transition ease-in-out duration-300 cursor-pointer">
                             </li>
                         </div>
 
-                        <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                        <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                             <a class="flex items-center gap-x-2 py-1 px-3 fill-verde-400  font-bold"
                                 href="supervisores.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -125,11 +125,11 @@ transition ease-in-out duration-300 cursor-pointer">
                                     <path
                                         d="M10 9a3 3 0 100-6 3 3 0 000 6zM6 8a2 2 0 11-4 0 2 2 0 014 0zM1.49 15.326a.78.78 0 01-.358-.442 3 3 0 014.308-3.516 6.484 6.484 0 00-1.905 3.959c-.023.222-.014.442.025.654a4.97 4.97 0 01-2.07-.655zM16.44 15.98a4.97 4.97 0 002.07-.654.78.78 0 00.357-.442 3 3 0 00-4.308-3.517 6.484 6.484 0 011.907 3.96 2.32 2.32 0 01-.026.654zM18 8a2 2 0 11-4 0 2 2 0 014 0zM5.304 16.19a.844.844 0 01-.277-.71 5 5 0 019.947 0 .843.843 0 01-.277.71A6.975 6.975 0 0110 18a6.974 6.974 0 01-4.696-1.81z" />
                                 </svg>
-                                Supervisor
+                                Supervisores
                             </a>
                         </li>
 
-                        <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                        <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                             <a class="flex items-center gap-x-2 py-1 px-3 fill-verde-400  font-bold"
                                 href="empresas.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
@@ -138,22 +138,22 @@ transition ease-in-out duration-300 cursor-pointer">
                                         d="M1 2.75A.75.75 0 011.75 2h10.5a.75.75 0 010 1.5H12v13.75a.75.75 0 01-.75.75h-1.5a.75.75 0 01-.75-.75v-2.5a.75.75 0 00-.75-.75h-2.5a.75.75 0 00-.75.75v2.5a.75.75 0 01-.75.75h-2.5a.75.75 0 010-1.5H2v-13h-.25A.75.75 0 011 2.75zM4 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM4.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM8 5.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zM8.5 9a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1zM14.25 6a.75.75 0 00-.75.75V17a1 1 0 001 1h3.75a.75.75 0 000-1.5H18v-9h.25a.75.75 0 000-1.5h-4zm.5 3.5a.5.5 0 01.5-.5h1a.5.5 0 01.5.5v1a.5.5 0 01-.5.5h-1a.5.5 0 01-.5-.5v-1zm.5 3.5a.5.5 0 00-.5.5v1a.5.5 0 00.5.5h1a.5.5 0 00.5-.5v-1a.5.5 0 00-.5-.5h-1z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                Empresa
+                                Empresas
                             </a>
                         </li>
 
-                        <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                        <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                             <a class="flex items-center gap-x-2 py-1 px-3 fill-verde-400  font-bold" href="cidades.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                     class="fill-verde-400 size-5">
                                     <path
                                         d="M14.916 2.404a.75.75 0 01-.32 1.012l-.596.31V17a1 1 0 01-1 1h-2.26a.75.75 0 01-.75-.75v-3.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.5a.75.75 0 01-.75.75h-3.5a.75.75 0 010-1.5H2V9.957a.75.75 0 01-.596-1.372L2 8.275V5.75a.75.75 0 011.5 0v1.745l10.404-5.41a.75.75 0 011.012.32zM15.861 8.57a.75.75 0 01.736-.025l1.999 1.04A.75.75 0 0118 10.957V16.5h.25a.75.75 0 110 1.5h-2a.75.75 0 01-.75-.75V9.21a.75.75 0 01.361-.64z" />
                                 </svg>
-                                Cidade
+                                Cidades
                             </a>
                         </li>
 
-                        <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                        <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                             <a class="flex items-center gap-x-2 py-1 px-3 fill-verde-400  font-bold" href="cursos.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                     class="fill-verde-400 size-5">
@@ -161,11 +161,11 @@ transition ease-in-out duration-300 cursor-pointer">
                                         d="M9.664 1.319a.75.75 0 01.672 0 41.059 41.059 0 018.198 5.424.75.75 0 01-.254 1.285 31.372 31.372 0 00-7.86 3.83.75.75 0 01-.84 0 31.508 31.508 0 00-2.08-1.287V9.394c0-.244.116-.463.302-.592a35.504 35.504 0 013.305-2.033.75.75 0 00-.714-1.319 37 37 0 00-3.446 2.12A2.216 2.216 0 006 9.393v.38a31.293 31.293 0 00-4.28-1.746.75.75 0 01-.254-1.285 41.059 41.059 0 018.198-5.424zM6 11.459a29.848 29.848 0 00-2.455-1.158 41.029 41.029 0 00-.39 3.114.75.75 0 00.419.74c.528.256 1.046.53 1.554.82-.21.324-.455.63-.739.914a.75.75 0 101.06 1.06c.37-.369.69-.77.96-1.193a26.61 26.61 0 013.095 2.348.75.75 0 00.992 0 26.547 26.547 0 015.93-3.95.75.75 0 00.42-.739 41.053 41.053 0 00-.39-3.114 29.925 29.925 0 00-5.199 2.801 2.25 2.25 0 01-2.514 0c-.41-.275-.826-.541-1.25-.797a6.985 6.985 0 01-1.084 3.45 26.503 26.503 0 00-1.281-.78A5.487 5.487 0 006 12v-.54z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                Curso
+                                Cursos
                             </a>
                         </li>
 
-                        <li class="hover:bg-slate-50 rounded-md py-2 mx-2">
+                        <li class="hover:bg-slate-50 rounded-md py-3 mx-2">
                             <a class="flex items-center gap-x-2 py-1 px-3 fill-verde-400  font-bold" href="areas.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                                     class="fill-verde-400 size-5">
@@ -173,7 +173,7 @@ transition ease-in-out duration-300 cursor-pointer">
                                         d="M1 2.75A.75.75 0 011.75 2h16.5a.75.75 0 010 1.5H18v8.75A2.75 2.75 0 0115.25 15h-1.072l.798 3.06a.75.75 0 01-1.452.38L13.41 18H6.59l-.114.44a.75.75 0 01-1.452-.38L5.823 15H4.75A2.75 2.75 0 012 12.25V3.5h-.25A.75.75 0 011 2.75zM7.373 15l-.391 1.5h6.037l-.392-1.5H7.373zM13.25 5a.75.75 0 01.75.75v5.5a.75.75 0 01-1.5 0v-5.5a.75.75 0 01.75-.75zm-6.5 4a.75.75 0 01.75.75v1.5a.75.75 0 01-1.5 0v-1.5A.75.75 0 016.75 9zm4-1.25a.75.75 0 00-1.5 0v3.5a.75.75 0 001.5 0v-3.5z"
                                         clip-rule="evenodd" />
                                 </svg>
-                                Área
+                                Áreas
                             </a>
                         </li>
 
